96924d66383b59a02c1f52e25f3acf2f2e4aedd3
Laya Pullela <lpullela@ucsb.edu>
2023-09-20 21:57:03 -0700

    new pages

--- client/src/components/bryce.js
+++ client/src/components/bryce.js
@@ -9,13 +9,30 @@ function BrycePage() {
         <img src={bryceimg} alt="Blog post cover" />
       </div>
       <div className="content-container-page">
-        <h1 className="title">Blog Post Title</h1>
-        <p className="date">September 19, 2023</p>
+        <h1 className="title">Bryce Canyon</h1>
+        <p className="date">September 17, 2023</p>
         <div className="body-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, a?</p>
-          <p>Quisquam consectetur dolores soluta aliquam? Odio, earum est?</p>
-          <p>Quidem consectetur numquam sapiente vero aliquid debitis?</p>
-          <p>Fugit assumenda quis reiciendis natus sit dolore aperiam repellendus labore!</p>
+          <p>The Native Paiute Indians who once inhabited the mountains of Southwestern Utah conjectured a punishment for those who commited "bad deeds"-- turning into a hoodoo.</p>
+          <p>Atop the peaks of the gorgeous Colorado Plateau in Utah is Bryce Canyon National Park, featuring the largest collection of towering, sharp, red sandstone natural sculptures, known as hoodoos.  </p>
+          <p>Visitors from all over the world come to visit Bryce to see this amphetheater of natural creation</p>
+          <p>But it's not only the hoodoos that contribute to Bryce Canyon's beauty and uniqueness. The surrounding greenery of the Colorado plateau with flowing water, in the backdrop of purple peaks makes Bryce a remote gem.</p>
+
+          <p>Many people visit Zion and Bryce together in the same trip. These parks, however, are vastly different. Zion is much more popular, featuring a village center under the park, and ample big name hotels. The climate is semi-arid and in only ~4000 feet altitude.</p>
+          <p>Bryce on the other hand is far more remote. It towers at ~8,000 feet on the mountain tops of rural Southwest Utah. There are no big name hotels in the area, only log cabins embedded within the pine trees of the park, and a few hotels/cabins outside the park in nearby hamlets/cities.</p>
+
+          <h3>How to get to Bryce</h3> 
+          <p>If you come from the South, fly into Las Vegas (more options) or Saint George airport. As you drive up the 15 freeway, you can knock out Zion National Park as well. You must rent a car for this trip.</p>
+          <p>The other option is to fly into Salt Lake City airport. The drive is only 4 hours, and you have the option of hitting several national parks this way: Zion, Canyonlands, and Arches.</p>
+
+          <h3>Where to stay</h3>
+          <p>Like Zion, Bryce has a lodge operated by the NPS, which is the most convenient/popular place to stay for tourists. From here, parking is easy, and reaching the hiking trails/outlook points via tram is a breeze. On our trip, we stayed in a nearby small town called Tropic at the Bryce Log Cabins. The stay was incredibly peaceful and we had no complains of the residence. There was a gas station just across the street, convenience store, and 2-3 dinner stops and one coffee/pastry shop. The drive to Bryce was approximately ~10 minutes from Tropic.</p>
+          <a href="https://www.visitbrycecanyon.com/">The Lodge at Bryce Canyon (in park)</a>
+          <p></p>
+          <a href="https://www.brycecanyonlogcabins.com/en-US">Bryce Log Cabins (Tropic, UT)</a>
+
+          <h3>Hikes</h3>
+          <p>One of the most reccomended itinery at Bryce was watching the sunsise, which tourists swore was a spectacular sight (worth waking up for!) There is a dedicated point at the park called Sunrise point where you can view the Hoodos as their peaks get illuminated by the rising sun. If it's early enough in the morning, you may be able to find parking by the Zion lodge and walk to the Sunrise point, but if not, you can take a tram from the visitor center, reach the sunset point, and walk down about a half mile to reach the Sunrise viewpoint. Many tourists come to see the sunrise, so you may meet a crowd.</p>
+          <p>As for the hikes, the pamplet at the visitor center outlines ~6 hikes around the canyon. No matter which you take, I highly reccomend descending down into the amphetheater to see the hoodoos at the ground level. You get a real sense of just how grand and magnificent they are looking up. However, the ascent back up to the rim (+1000 feet) may be challenging, especially in altitude. If you prefer, the Rim Trail presents a wonderful top level view of the amphetheater from every angle on the road-side of the canyon. It's a beautiful option, but again, I highly reccomend descending into the canyon. </p>
         </div>
       </div>
     </div>
--- client/src/components/zion.js
+++ client/src/components/zion.js
@@ -9,13 +9,25 @@ function ZionPage() {
         <img src={zionimg} alt="Blog post cover" />
       </div>
       <div className="content-container-page">
-        <h1 className="title">Blog Post Title</h1>
-        <p className="date">September 19, 2023</p>
+        <h1 className="title">Zion National Park</h1>
+        <p className="date">September 16, 2023</p>
         <div className="body-text">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, a?</p>
-          <p>Quisquam consectetur dolores soluta aliquam? Odio, earum est?</p>
-          <p>Quidem consectetur numquam sapiente vero aliquid debitis?</p>
-          <p>Fugit assumenda quis reiciendis natus sit dolore aperiam repellendus labore!</p>
+          <p>Nestled between the Grand Canyon towards the South and Bryce Canyon running North is the oasis of Zion National Park in Southwestern Utah.</p>
+          <p>Towering, vertical canyons of gorgeous red rock are cut by the Virgin River and its creeping tributaries, forming a deep, narrow gorge where wildlife pervades.</p>
+          <p>Zion has become a popular stop for tourists, and it's not for no reason!</p>
+          <h3>Getting to the park</h3>
+          <p>You have a few options. If you fly/drive into Las Vegas airport and rent a car (must), Zion is only a ~2.5 hour drive. Flights to Vegas are cheaper, and this way you can also spend a few days in Sin City towards the beginning and/or end of your trip. On top of that, Vegas is the central point between Utah's parks and the great Grand Canyon (2.5 hours away).</p>
+          <p>If you are not interested in spending a day or two in Vegas, and willing to spend a little bit of extra money, you can also fly into Saint George airport in Utah, which is less than an hour from the national park. You won't be able to git Grand Canyon (over 5 hour drive), but Bryce and Zion are convenient.</p>
+          <p>Your final option is flying into Salt Lake City, and driving to Zion (4 hours). Bryce is along the same route and just under two hours from Zion. This time on our trip, we took the Vegas route, but we hope to fly into Salt Lake City on the next visit and hit Arches and Canyonlands.</p>
+          <p></p>
+          <h3>Where to stay</h3>
+          <p>The most convenient place to stay is Zion Lodge, but you must book early. Zion lodge is operated by the National Park Service itself, and thus the only place to stay within the main park. In fact, cars are not allowed to run through the main canyon, unless you are driving to stay at the lodge. All other transportation is via a tram service that runs through 9 stops every ~10 minutes. I recommend this option for big groups and families. You will always be in proximity to the lodge and tram, and it makes it very easy for members of your party to split up, take a break, and relax, while others can continue hiking.</p>
+          <a href="https://www.zionlodge.com/about-us/">Zion Lodge</a>
+          <p>If the Zion lodge doesn't work out, you can stay in the nearly hotels/cabins outside the national park. Expect to drive ~10-30 minutes to reach the park. We stayed in the Fairfield Marriot 10 miles down the mountain, and it was beautiful. The only issue is parking at the visitor center, especially if you drive up between 9-10 or during the weekends/peak visiting seasons. I would consider parking under the enterance of the national park gates, walking up to the visitor center (~half mile), and then taking the tram up to the trail heads. You can try to road-side park along the main road up the canyon, but there are very few spots.</p>
+          <h3>Getting around the park</h3>
+          <p>The national park is organized with 9 stops, the first starting down at the visitor center, and the final stopping at the top of the main road through the canyon, where the famous Narrows hike begins. At the gate of the national park, you will recieve a pamphlet describing the different hikes which begin at the trailheads along those 9 stops. The most iconic hikes are the following: Narrows, Angels Landing, Scouts Landing... these hikes are a bit challenging because you need to ascend 1500+ feet to reach the top of the canyon. But you'll get the iconic birdseye views and stunning photographs of Zion from these points.</p>
+          <p>Other popular hikes included the Emerald Pools hike and the Watchman trail which feature views from lower elevation and may feature more wildlife spotting around the river (deer, rams, etc.) </p>
+          <p meta charset="UTF-8">To complete the Angels Landing hike, you'll need to get a permit at least one day in advance (I didn't know this going in &#x1F614;). The Angels Landing is one of the most popular hikes in the USA, a little bit dangerous on the steep climb with open, unfenced cliffs on both sides, but (from what I heard) super worth it. When I visit again, I'll be sure to do this climb.</p>
         </div>
       </div>
     </div>
